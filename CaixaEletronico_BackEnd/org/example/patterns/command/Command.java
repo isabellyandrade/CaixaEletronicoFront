c0472d9ff3b161d7d5329a9c4e6c3d79a6184666
isabellyandrade <137850120+isabellyandrade@users.noreply.github.com>
2025-07-01 21:48:10 -0300

    Delete CaixaEletronico_BackEnd directory

--- CaixaEletronico_BackEnd/org/example/patterns/command/Command.java
+++
@@ -1,9 +0,0 @@
-package org.example.patterns.command;
-
-/**
- * PATTERN: Command (Interface)
- * Declara uma interface para executar uma operação.
- */
-public interface Command {
-    boolean execute();
-}
